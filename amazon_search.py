7578ad6b72c1814166a379273ab33e657c232bf0
Neetu Pundir <neetu.pundir@outlook.com>
2021-05-08 14:25:51 +0200

    add file

---
+++ amazon_search.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 24 12:25:41 2021
+
+@author: win10
+"""
+
+from autoscraper import AutoScraper
+amazon_url="https://www.amazon.in/s?k=iphones"
+
+wanted_list=["₹58,400","New Apple iPhone 11 (128GB) - Black"]
+
+scraper=AutoScraper()
+result=scraper.build(amazon_url,wanted_list)
+
+print(scraper.get_result_similar(amazon_url,grouped=True))
+
+
+
+scraper.set_rule_aliases({'rule_io1c':'Title','rule_hm52': 'Price'})
+scraper.keep_rules(['rule_io1c','rule_hm52'])
+scraper.save('AmazonIn-search')
+
